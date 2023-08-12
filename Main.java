560e788fbd4b6838bad22b041f214b9563ee84f6
John Andre Jabon <142065015+JohnTheBomb7416@users.noreply.github.com>
2023-08-11 22:17:21 -0700

    Code
    Hello World
    John was here

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main 
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello World blah blah blah John was here"); 
+      System.out.println("YO MAMA");
+      //john was here
+      
+   }
+   
+   
+}
